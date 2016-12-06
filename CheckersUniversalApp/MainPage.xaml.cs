2062487c07e35cae316f2ba001ced1366ed94a99
Jose Otmaro Gonzalez Esquivel <jesquiuelgonzalez@harding.edu>
2016-12-06 17:55:43 -0600

    Board prototype

--- CheckersUniversalApp/MainPage.xaml.cs
+++ CheckersUniversalApp/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -24,7 +25,8 @@ namespace CheckersUniversalApp
     {
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+          
         }
     }
 }
