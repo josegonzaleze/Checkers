0c4cfda588e7ba72fc459d01d5b7aeb13228b006
Jose Otmaro Gonzalez Esquivel <jesquiuelgonzalez@harding.edu>
2016-12-06 20:53:20 -0600

    Board Game Update

--- CheckersUniversalApp/MainPage.xaml.cs
+++ CheckersUniversalApp/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,7 +27,8 @@ namespace CheckersUniversalApp
         public MainPage()
         {
             InitializeComponent();
-          
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
+
         }
     }
 }
